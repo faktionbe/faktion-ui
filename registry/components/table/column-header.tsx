ab05f56f62b4b0f0c5681970ba2ab898b042361f
Indy Van Canegem <i.vancanegem@faktion.com>
2025-11-18 13:43:29 +0100

    fix: inline sorting

--- registry/components/table/column-header.tsx
+++ registry/components/table/column-header.tsx
@@ -10,12 +10,6 @@ import {
 } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { cn } from '@/lib/utils';
 
 interface ColumnHeaderProps<TData, TValue>
@@ -37,39 +31,22 @@ export const ColumnHeader = <TData, TValue>({
 
   return (
     <div className={cn('flex items-center space-x-2', className)}>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button
-            variant='ghost'
-            size='sm'
-            className='data-[state=open]:bg-accent -ml-3 h-8'>
-            <span>{title}</span>
-            {column.getIsSorted() === 'desc' ? (
-              <ChevronDown />
-            ) : column.getIsSorted() === 'asc' ? (
-              <ChevronUp />
-            ) : (
-              <ChevronsUpDown />
-            )}
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align='start'>
-          <DropdownMenuItem
-            onClick={() => {
-              column.toggleSorting(false);
-            }}>
-            <ArrowUp className='text-muted-foreground/70 h-3.5 w-3.5' />
-            asc
-          </DropdownMenuItem>
-          <DropdownMenuItem
-            onClick={() => {
-              column.toggleSorting(true);
-            }}>
-            <ArrowDown className='text-muted-foreground/70 h-3.5 w-3.5' />
-            desc
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+      <Button
+        variant='ghost'
+        size='sm'
+        className='-ml-3 h-8'
+        onClick={() => {
+          column.toggleSorting();
+        }}>
+        <span>{title}</span>
+        {column.getIsSorted() === 'desc' ? (
+          <ChevronDown />
+        ) : column.getIsSorted() === 'asc' ? (
+          <ChevronUp />
+        ) : (
+          <ChevronsUpDown />
+        )}
+      </Button>
     </div>
   );
 };
